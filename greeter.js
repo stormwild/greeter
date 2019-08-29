dbfacd45394ac32da4713a72837730151871191d
Alexander Torrijos <artorrijos@gmail.com>
2019-08-29 21:58:16 +0800

    Code working

---
+++ greeter.js
@@ -0,0 +1,17 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+var user = new Student("Jane", "M.", "User");
+var el = document.createElement('h1');
+el.textContent = greeter(user);
+var app = document.getElementById('app');
+app.appendChild(el);
